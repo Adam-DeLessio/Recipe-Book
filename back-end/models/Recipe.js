e9f9028b8413afbea3f9aa57aa1f5056f9757c50
Adam DeLessio <adamdelessio@gmail.com>
2020-01-27 11:27:59 -0500

    removed original backend

--- back-end/models/Recipe.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('../db/connection')
-const Schema = mongoose.Schema
-
-const RecipeSchema = new Schema({
-
-})
-
-const Recipe = mongoose.model('Recipe', RecipeSchema)
-module.exports = Recipe
